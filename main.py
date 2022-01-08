ed0abf702ee270e5ad9ca287e0d13a8106ebbac6
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-08 17:29:37 +0200

    fixing output for the final file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-
+from os.path import splitext
 import PyPDF2
 from gtts import gTTS
-import os
+
 
 Tk().withdraw()
 file_location = askopenfilename()
@@ -11,7 +11,6 @@ file_location = askopenfilename()
 pdf_file = open(file_location, mode='rb')
 pdf_doc = PyPDF2.PdfFileReader(pdf_file)
 len_document = pdf_doc.numPages
-print(pdf_doc.numPages)
 
 text_to_read = ''
 
@@ -21,11 +20,8 @@ for page in range(len_document):
     text_to_read += a
 
 
-myobj = gTTS(text=text_to_read, lang='en', slow=False)
+final_file = gTTS(text=text_to_read, lang='en', slow=False)
 
-# Saving the converted audio in a mp3 file named
-# welcome
-myobj.save("welcome.mp3")
+out_name = splitext(file_location)[0] + '.mp3'
+final_file.save(out_name)
 
-# Playing the converted file
-# os.system("mpg321 welcome.mp3")
