dee5dcdbf8a33471b3a1306370d13f33b778e2b0
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-08 16:53:24 +0200

    link reading of pdf to gTTS

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from gtts import gTTS
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-# This module is imported so that we can
-# play the converted audio
+import PyPDF2
+from gtts import gTTS
 import os
 
+Tk().withdraw()
+file_location = askopenfilename()
 
-my_text = 'Примерен техт'
-language = 'bg'
+pdf_file = open(file_location, mode='rb')
+pdf_doc = PyPDF2.PdfFileReader(pdf_file)
+len_document = pdf_doc.numPages
+print(pdf_doc.numPages)
 
-myobj = gTTS(text=my_text, lang=language, slow=False)
+text_to_read = ''
+
+for page in range(len_document):
+    current_page = pdf_doc.getPage(page)
+    a = current_page.extractText()
+    text_to_read += a
+
+
+myobj = gTTS(text=text_to_read, lang='en', slow=False)
 
 # Saving the converted audio in a mp3 file named
 # welcome
 myobj.save("welcome.mp3")
 
 # Playing the converted file
-os.system("mpg321 welcome.mp3")
+# os.system("mpg321 welcome.mp3")
